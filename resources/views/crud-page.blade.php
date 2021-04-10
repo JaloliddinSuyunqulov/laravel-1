893ad8d259cc3f18bccb9fdb4e0579460100aad4
JaloliddinSuyunqulov <jaloliddinsuyunkulov@gmail.com>
2021-04-10 14:24:44 +0500

    Install Laravel

---
+++ resources/views/crud-page.blade.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Pagination\Paginator;
+public function boot(){
+    Paginator::useBootstrap();
+}
+?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud-Laravel-8</title>
+
+</head>
+<body>
+<div class="container">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cumque enim eum illum itaque, laborum
+    libero necessitatibus, odit praesentium quaerat quas quisquam quod sunt tempore tenetur vitae voluptatem? Ratione!
+</div>
+</body>
+</html>
